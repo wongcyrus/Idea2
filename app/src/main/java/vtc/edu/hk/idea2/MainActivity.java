5813b78c4dab106171fcd1d9e5605b90d083d035
wongcyrus <cytmp-git@yahoo.com>
2015-06-03 09:16:29 +0000

    Change the RailGirl services with call back. Code to create new idea record.

--- app/src/main/java/vtc/edu/hk/idea2/MainActivity.java
+++ app/src/main/java/vtc/edu/hk/idea2/MainActivity.java
@@ -1,18 +1,65 @@
 package vtc.edu.hk.idea2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import vtc.edu.hk.idea2.api.RailGirlService;
+import vtc.edu.hk.idea2.data.IdeaItem;
 
 
 public class MainActivity extends Activity {
 
-    public void onClick (View view) {
+    private static final String API = "http://10.0.2.2:3000/";
 
-        Toast.makeText(this, "Saved",
+    public void onClick(View view) {
+
+        //Retrofit section start from here...
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API).build();                                        //create an adapter for retrofit with base url
+
+        RailGirlService railGirlService = restAdapter.create(RailGirlService.class);
+
+        IdeaItem ideaItem = new IdeaItem();
+        EditText input = (EditText) findViewById(R.id.name);
+        String name = input.getText().toString();
+        ideaItem.setName(name);
+
+        input = (EditText) findViewById(R.id.descriptions);
+        String descriptions = input.getText().toString();
+        ideaItem.setDescription(descriptions);
+
+        input = (EditText) findViewById(R.id.picture);
+        String picture = input.getText().toString();
+        ideaItem.setPicture(picture);
+
+        final Context applicationContext = getApplicationContext();
+        railGirlService.createIdea(ideaItem, new Callback<IdeaItem>() {
+
+            @Override
+            public void success(IdeaItem ideaItem, Response response) {
+                Log.i(this.getClass().getName(), "Save Success!");
+                Toast.makeText(applicationContext, "Save Success!", Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.i(this.getClass().getName(), "Save Unsuccess!" + error);
+                Toast.makeText(applicationContext, "Save Unsuccess!", Toast.LENGTH_LONG);
+            }
+        });
+
+        Toast.makeText(this, "Sent Save Request!",
                 Toast.LENGTH_LONG).show();
     }
 
--- app/src/main/java/vtc/edu/hk/idea2/api/RailGirlService.java
+++ app/src/main/java/vtc/edu/hk/idea2/api/RailGirlService.java
@@ -1,11 +1,13 @@
 package vtc.edu.hk.idea2.api;
 
+import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.POST;
 import vtc.edu.hk.idea2.data.IdeaItem;
 
 //http://square.github.io/retrofit/
+//http://themakeinfo.com/2015/04/retrofit-android-tutorial/
 public interface RailGirlService {
-    @POST("/ideas/new")
-    void createIdea(@Body IdeaItem idea);
+    @POST("/ideas.json")
+    void createIdea(@Body IdeaItem ideaItem, Callback<IdeaItem> response);
 }
